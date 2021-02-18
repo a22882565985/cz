217583e2471a78b63c0534349a9ed91441b83748
chenzhe <chenzhe@163.com>
2021-02-18 15:52:24 +0800

    sayHi

---
+++ src/test/java/com/turing/cz/CzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turing.cz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
